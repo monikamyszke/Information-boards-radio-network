831807a86a69e551f79fa18bd2369ab26e765c03
Monika <s160435@student.pg.edu.pl>
2018-11-07 20:45:07 +0100

    Receiving_Node Refactoring

--- Receiving_Node/src/Bluetooth.java
+++ Receiving_Node/src/Bluetooth.java
@@ -1,9 +1,7 @@
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
@@ -16,96 +14,100 @@ import org.apache.commons.io.IOUtils;
 
 public class Bluetooth {
 	
-	StreamConnectionNotifier notifier;
-	StreamConnection conn;
-	File receivedFile;
-	String fileName;
-	byte[] fileNameBytes;
-	byte[] bytesArray;
-	byte[] frame;
-	String responseAddress;
-	byte[] ack;
+	private StreamConnectionNotifier notifier;
+	private StreamConnection conn;
+	private byte[] ack;
 
-	RemoteDevice device;
-	
 	public void run() {
 		
 		try {
 			System.out.println("Nas³uchujê");
-			notifier = (StreamConnectionNotifier)Connector.open("btspp://localhost:" + new UUID( 0x1101 ).toString( ));//obiekt oczekuj¹cy po³¹czenia przychodz¹cego do serwera, reprezentuje nas³uchuj¹ce gniazdo
+			
+			// obiekt oczekuj¹cy po³¹czenia przychodz¹cego do serwera, reprezentuje nas³uchuj¹ce gniazdo
+			notifier = (StreamConnectionNotifier)Connector.open("btspp://localhost:" + new UUID(0x1101).toString());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		while(true) {
+		while (true) {
 			listenForConnection();
 		}
 	}
 	
 	public void listenForConnection() {
 		try {
-			conn = (StreamConnection)notifier.acceptAndOpen(); //akceptacja i ustanowienie po stronie serwera po³¹czenia przychodz¹cego od klienta
+			String responseAddress;
+			// akceptacja i ustanowienie po stronie serwera po³¹czenia przychodz¹cego od klienta
+			conn = (StreamConnection)notifier.acceptAndOpen(); 
 			System.out.println("Otwarto po³¹czenie");
 			RemoteDevice device = RemoteDevice.getRemoteDevice(conn);
 			responseAddress = "btspp://" + device.getBluetoothAddress() + ":6"; 
-			receiveData();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void receiveData() {
-		InputStream is;
-		try {
-			is = conn.openInputStream(); //otwarcie strumienia wejœciowego danych
-			frame = IOUtils.toByteArray(is); //zapisanie danych ze strumienia do tablicy bajtów
-			is.close();
-			if(frame.length == 0) {
-				System.out.println("It's a ping!");
-				conn.close();
-			}
-			else {
-				System.out.println("Odebrano plik");
-				conn.close();
-//				saveBytesToFile();
-				decodeFrame();
-				saveFile();
-				
+			
+			if (receiveData() == true) {
 				sendResponse(responseAddress);
 			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void decodeFrame() {
+	public boolean receiveData() {
+		InputStream is;
+		byte[] frame;
+		boolean isFile = false;
+		
+		try {
+			is = conn.openInputStream(); // otwarcie strumienia wejœciowego danych
+			frame = IOUtils.toByteArray(is); // zapisanie danych ze strumienia do tablicy bajtów
+			is.close();
+			conn.close();
+			if (frame.length == 0) {
+				System.out.println("It's a ping!");
+			} else {
+				System.out.println("Odebrano plik");
+				decodeFrameAndSave(frame);
+				isFile = true;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return isFile;
+	}
+	
+	public void decodeFrameAndSave(byte[] frame) {
+		String filename = null;
+		byte[] filenameBytes;
+		byte[] bytesArray;
 		byte[] numberOfBytesArray = new byte[4];
+		
 		System.arraycopy(frame, 0, numberOfBytesArray, 0, 4);
 		int numberOfBytes = ByteBuffer.wrap(numberOfBytesArray).getInt();
-		int fileNameLength = frame[4];
-		fileNameBytes = new byte[fileNameLength];
-		bytesArray = new byte[frame.length - fileNameLength - 5];
-		System.arraycopy(frame, 5, fileNameBytes, 0, fileNameLength);
+		int filenameLength = frame[4];
+		filenameBytes = new byte[filenameLength];
+		bytesArray = new byte[frame.length - filenameLength - 5];
+		System.arraycopy(frame, 5, filenameBytes, 0, filenameLength);
 		try {
-			fileName = new String(fileNameBytes, "UTF-8");
+			filename = new String(filenameBytes, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		System.arraycopy(frame, fileNameLength + 5, bytesArray, 0, bytesArray.length);
-		System.out.println("Zdekodowano ramkê. Liczba bajtów ramki w polu nag³ówka: " + (numberOfBytes));
+		System.arraycopy(frame, filenameLength + 5, bytesArray, 0, bytesArray.length);
+		System.out.println("Zdekodowano ramkê. Liczba bajtów ramki w polu nag³ówka: " + numberOfBytes);
 		System.out.println("D³ugoœæ ramki: " + frame.length);
 		ack = new byte[1];
-		if(frame.length == numberOfBytes) {
+		if (frame.length == numberOfBytes) {
 			System.out.println("Dane odebrano poprawnie");
 			ack[0] = 1;
-		}
-		else {
+			saveFile(filename, bytesArray);
+		} else {
 			System.out.println("Podczas po³¹czenia wyst¹pi³ b³¹d");
 			ack[0] = 0;
 		}
 	}
 	
-	public void saveFile() {
+	public void saveFile(String fileName, byte[] bytesArray) {
 		FileOutputStream os;
 		try {
 			os = new FileOutputStream("/home/pi/Desktop/" + fileName);
@@ -116,46 +118,29 @@ public class Bluetooth {
 			e.printStackTrace();
 		}
 	}
-	
-	//funkcja pomocnicza - zapisanie ramki w postaci heksalnej do pliku tekstowego
-	public void saveBytesToFile() {
-		PrintWriter bytesWriter;
-		try {
-			File bytesTxtFile = new File("/home/pi/Desktop/receivedBytes.txt");
-			bytesWriter = new PrintWriter(bytesTxtFile);
-			for(byte b : frame) {
-				bytesWriter.print(String.format("0x%02X ", b));
-			}
-			bytesWriter.close();
-			System.out.println("Zapisano bajty do pliku tekstowego");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
 	public void sendResponse(String address) {
-			StreamConnection conn1;
+		StreamConnection responseConn;
 
-				try {
-					try {
-						TimeUnit.MILLISECONDS.sleep(100); //opóznienie w wys³aniu odpowiedzi
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					conn1 = (StreamConnection) Connector.open(address);
-					OutputStream os = conn1.openOutputStream();
-					os.write(ack);
-					os.close();
-					try {
-						TimeUnit.MILLISECONDS.sleep(100);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					conn1.close();
-					System.out.println("Wys³ano odpowiedz");
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
+		try {
+			try {
+				TimeUnit.MILLISECONDS.sleep(100); //opóznienie w wys³aniu odpowiedzi
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			responseConn = (StreamConnection) Connector.open(address);
+			OutputStream os = responseConn.openOutputStream();
+			os.write(ack);
+			os.close();
+			try {
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			responseConn.close();
+			System.out.println("Wys³ano odpowiedz");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
-
 }
--- Receiving_Node/src/Main.java
+++ Receiving_Node/src/Main.java
@@ -1,26 +1,13 @@
-//import java.awt.event.WindowAdapter;
-//import java.awt.event.WindowEvent;
-
 import java.io.IOException;
+
 public class Main {
-	
-	static AppWindow frame;
 	
 	public static void main(String[] args) {
 		
 		Bluetooth bluetooth = new Bluetooth();
-		
-		//utworzenie okna aplikacji
-//		try {
-//			frame = new AppWindow();
-//			frame.setVisible(true);
-//			frame.setResizable(true);
-//			frame.setSize(1000, 600);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-	
-		//zmiana trybu widocznoœci urz¹dzenia tak, aby by³o ono mo¿liwe do wykrycia przez ca³y czas dzia³ania aplikacji
+
+		// zmiana trybu widocznoœci urz¹dzenia tak, aby by³o ono mo¿liwe do wykrycia przez ca³y czas dzia³ania aplikacji
+		// TODO: sprawdziæ czy sudo jest konieczne
 		String[] discoverableCommand = new String[] {"sudo", "hciconfig", "hci0", "piscan"};
 		try {
 			new ProcessBuilder(discoverableCommand).start();
@@ -28,9 +15,8 @@ public class Main {
 			e2.printStackTrace();
 		}
 		
-		//wy³¹czenie widocznoœci urz¹dzenia w momencie zakoñczenie dzia³ania programu
+		// wy³¹czenie widocznoœci urz¹dzenia w momencie zakoñczenie dzia³ania programu
 		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-			@Override
 			public void run() {
 				String[] nonDiscoverableCommand = new String[] {"sudo", "hciconfig", "hci0", "noscan"};
 				try {
@@ -39,12 +25,9 @@ public class Main {
 					e2.printStackTrace();
 				}
 			}
-			
-		})
-		);
+		}));
 		
 		bluetooth.run();
 
 	}
-
 }
